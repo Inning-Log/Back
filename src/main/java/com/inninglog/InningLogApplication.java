9c3efc41971cc298fc3bf5f80785577702849b58
YoonBoorue <boorue03@gmail.com>
2026-07-06 17:06:52 +0900

    chore : initialize setting

---
+++ src/main/java/com/inninglog/InningLogApplication.java
@@ -0,0 +1,13 @@
+package com.inninglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InningLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InningLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inninglog/InningLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inninglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InningLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
